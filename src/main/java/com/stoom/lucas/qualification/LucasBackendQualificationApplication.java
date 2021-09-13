a6acd24649f7f2de176179a0c33876e03df705fc
lucasmends <lucasmends93@gmail.com>
2021-09-13 16:47:26 -0300

    feat: spring boot configuration

---
+++ src/main/java/com/stoom/lucas/qualification/LucasBackendQualificationApplication.java
@@ -0,0 +1,13 @@
+package com.stoom.lucas.qualification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasBackendQualificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasBackendQualificationApplication.class, args);
+	}
+
+}
